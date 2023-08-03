04df906cc4538b84dee066e8057a465f7488453d
Bogdan <thegarrybit@gmail.com>
2023-08-03 14:49:40 +0300

    feat: add css modules

---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,12 @@
+import AppNav from '../components/AppNav.jsx';
+
+function AppLayout() {
+  return (
+    <div>
+      <AppNav />
+      <h1>app</h1>
+    </div>
+  );
+}
+
+export default AppLayout;
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,14 +1,16 @@
 import { Link } from 'react-router-dom';
 
 import PageNav from '../components/PageNav.jsx';
+import AppNav from '../components/AppNav.jsx';
 
 function Homepage() {
   return (
     <div>
+      <AppNav />
       <PageNav />
       <h1>WorldWise</h1>
 
-      <Link to="/pricing">Pricing</Link>
+      <Link to="/app">Go to the app</Link>
     </div>
   );
 }
